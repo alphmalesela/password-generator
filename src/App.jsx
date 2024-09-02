b64f726d52d5deb3762c6ecb98b46b1201838cb5
Malesela Seabi <seabiali@gmail.com>
2024-09-02 16:56:13 +0200

    Generate and copy password

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,127 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [range, setRange] = useState(0)
+  const [uppercase, setUppercase] = useState(false)
+  const [lowercase, setLowercase] = useState(false)
+  const [numbers, setNumbers] = useState(false)
+  const [symbols, setSymbols] = useState(false)
+  const [password, setPassword] = useState("P4$5W0rD!")
+
+  const [strength, setStrength] = useState(0)
+
+  const onRangeChange = (e) => {
+    setRange(e.target.value)
+  }
+
+  const onToggleUppercase = () => {
+    setUppercase(!uppercase)
+  }
+
+  const onToggleLowercase = () => {
+    setLowercase(!lowercase)
+  }
+
+  const onToggleNumbers = () => {
+    setNumbers(!numbers)
+  }
+
+  const onToggleSymbols = () => {
+    setSymbols(!symbols)
+  }
+
+  const generatePassword = () => {
+    var chars = "";
+
+    if (uppercase) {
+      chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    }
+
+    if (lowercase) {
+      chars += "abcdefghijklmnopqrstuvwxyz"
+    }
+
+    if (numbers) {
+      chars += "0123456789";
+    }
+
+    if (symbols) {
+      chars += "!@#$%^&*()"
+    }
+
+    var random = 0
+    var pass = ""
+
+    for (var i = 0; i < range; i++) {
+      random = Math.floor(Math.random() * chars.length)
+      pass += chars.substring(random, random + 1)
+    }
+
+    setPassword(pass)
+  }
+
+  const copyPasswordToClipboard = () => {
+    if (navigator && navigator.clipboard && navigator.clipboard.writeText) {
+      navigator.clipboard.writeText(password);
+      alert('Copied password to clipboard')
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <p>Password Generator</p>
+      <div class="generator">
+        <div class="password-wrap">
+          <span id='password'>{password}</span>
+          <span class="icon" onClick={() => copyPasswordToClipboard()}>copy</span>
+        </div>
+        <div className="card">
+          <div>
+            <label for="uppercase">Character Length</label>
+            <input
+              defaultValue={0}
+              onChange={onRangeChange} type="range" id="length" name="length" min="0" max="20" />
+          </div>
+          <div>
+            <label for="uppercase">Include Uppercase Letters</label>
+            <input
+              onChange={onToggleUppercase}
+              type="checkbox"
+              id="uppercase"
+              name="uppercase" />
+          </div>
+          <div>
+            <label for="lowercase">Include Lowercase Letters</label>
+            <input
+              onChange={onToggleLowercase}
+              type="checkbox"
+              id="lowercase"
+              name="lowercase" />
+          </div>
+          <div>
+            <label for="numbers">Include Numbers</label>
+            <input
+              onChange={onToggleNumbers}
+              type="checkbox"
+              id="numbers"
+              name="numbers" />
+          </div>
+          <div>
+            <label for="symbols">Include Symbols</label>
+            <input
+              onChange={onToggleSymbols}
+              type="checkbox"
+              id="symbols"
+              name="symbols" />
+          </div>
+          <div>
+          </div>
+          <button onClick={() => generatePassword()}>
+            GENERATE
+          </button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
