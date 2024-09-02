fea64a618d02c6dc34ed0dfe5d246a78aed3258b
Malesela Seabi <seabiali@gmail.com>
2024-09-02 19:33:08 +0200

    Add bootsrap and calculate strength

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -13,6 +11,7 @@ function App() {
   const [password, setPassword] = useState("P4$5W0rD!")
 
   const [strength, setStrength] = useState(0)
+  const [strengthLabel, setStrengthLabel] = useState("")
 
   const onRangeChange = (e) => {
     setRange(e.target.value)
@@ -36,21 +35,26 @@ function App() {
 
   const generatePassword = () => {
     var chars = "";
+    var localStrength = 0
 
     if (uppercase) {
       chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+      localStrength += 5
     }
 
     if (lowercase) {
       chars += "abcdefghijklmnopqrstuvwxyz"
+      localStrength += 5
     }
 
     if (numbers) {
-      chars += "0123456789";
+      chars += "0123456789"
+      localStrength += 5
     }
 
     if (symbols) {
       chars += "!@#$%^&*()"
+      localStrength += 5
     }
 
     var random = 0
@@ -61,7 +65,38 @@ function App() {
       pass += chars.substring(random, random + 1)
     }
 
+    const totalStrength = Number(range) + Number(localStrength)
+    console.log("range ", range)
+    console.log("localStrength ", localStrength)
+    console.log("totalStrength ", totalStrength)
+
+    const percentage = (totalStrength / 40) * 100
+    console.log("percentage  ", percentage)
     setPassword(pass)
+    percentageStrength(percentage)
+  }
+
+  const percentageStrength = (strength) => {
+    if (strength > 74) {
+      setStrength(3)
+      setStrengthLabel("STRONG")
+      return
+
+    }
+    if (strength > 49) {
+      setStrength(2)
+      setStrengthLabel("MEDIUM")
+      return
+    }
+    if (strength > 24) {
+      setStrength(1)
+      setStrengthLabel("WEAK")
+      return
+    }
+
+    setStrength(0)
+    setStrengthLabel("TOO WEAK!")
+
   }
 
   const copyPasswordToClipboard = () => {
@@ -73,58 +108,77 @@ function App() {
 
   return (
     <>
-      <p>Password Generator</p>
-      <div class="generator">
-        <div class="password-wrap">
+      <p class="pass-gen-grey">Password Generator</p>
+      <div class="card password-wrap">
+        <div class="card-body">
           <span id='password'>{password}</span>
           <span class="icon" onClick={() => copyPasswordToClipboard()}>copy</span>
         </div>
-        <div className="card">
-          <div>
-            <label for="uppercase">Character Length</label>
-            <input
-              defaultValue={0}
-              onChange={onRangeChange} type="range" id="length" name="length" min="0" max="20" />
+      </div>
+      <ul class="list-group password-wrap">
+        <li class="list-group-item character-length">
+          <label for="uppercase">Character Length</label>
+          <label class="range">{range}</label>
+        </li>
+        <li class="list-group-item">
+          <input
+            class="range-input"
+            defaultValue={0}
+            onChange={onRangeChange} type="range" id="length" name="length" min="0" max="20" />
+        </li>
+        <li class="list-group-item">
+          <input
+            class="form-check-input me-1"
+            onChange={onToggleUppercase}
+            type="checkbox"
+            id="uppercase"
+            name="uppercase" />
+          <label class="form-check-label" for="uppercase">Include Uppercase Letters</label>
+        </li>
+        <li class="list-group-item">
+          <input
+            class="form-check-input me-1"
+            onChange={onToggleLowercase}
+            type="checkbox"
+            id="lowercase"
+            name="lowercase" />
+          <label class="form-check-label" for="lowercase">Include Lowercase Letters</label>
+        </li>
+        <li class="list-group-item">
+          <input
+            class="form-check-input me-1"
+            onChange={onToggleNumbers}
+            type="checkbox"
+            id="numbers"
+            name="numbers" />
+          <label class="form-check-label" for="numbers">Include Numbers</label>
+        </li>
+        <li class="list-group-item">
+          <input
+            class="form-check-input me-1"
+            onChange={onToggleSymbols}
+            type="checkbox"
+            id="symbols"
+            name="symbols" />
+          <label class="form-check-label" for="symbols">Include Symbols</label>
+        </li>
+        <li class="list-group-item">
+          <div class="card strength-card">
+            <div class="card-body strength-wrap">
+              <span class="strength-label">STRENGTH</span>
+              <span class="strength">{strengthLabel} {strength}</span>
+            </div>
           </div>
-          <div>
-            <label for="uppercase">Include Uppercase Letters</label>
-            <input
-              onChange={onToggleUppercase}
-              type="checkbox"
-              id="uppercase"
-              name="uppercase" />
-          </div>
-          <div>
-            <label for="lowercase">Include Lowercase Letters</label>
-            <input
-              onChange={onToggleLowercase}
-              type="checkbox"
-              id="lowercase"
-              name="lowercase" />
-          </div>
-          <div>
-            <label for="numbers">Include Numbers</label>
-            <input
-              onChange={onToggleNumbers}
-              type="checkbox"
-              id="numbers"
-              name="numbers" />
-          </div>
-          <div>
-            <label for="symbols">Include Symbols</label>
-            <input
-              onChange={onToggleSymbols}
-              type="checkbox"
-              id="symbols"
-              name="symbols" />
-          </div>
-          <div>
-          </div>
-          <button onClick={() => generatePassword()}>
+        </li>
+        <li class="list-group-item">
+          <button
+            type='button'
+            class="btn btn-success btn-gen"
+            onClick={() => generatePassword()}>
             GENERATE
           </button>
-        </div>
-      </div>
+        </li>
+      </ul>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+//import 'bootstrap/dist/css/bootstrap.css';
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
